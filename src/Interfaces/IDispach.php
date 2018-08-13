707c8faafed1fe574db3530a889158bf17d42d64
Joao Moraes <joaomoraesbr@gmail.com>
2018-08-12 21:47:19 -0300

    add interface

--- src/Interfaces/IDispach.php
+++ src/Interfaces/IDispach.php
@@ -2,8 +2,37 @@
 
 namespace FcPhp\Dispach\Interfaces
 {
+    use FcPhp\Di\Interfaces\IDi;
+    
     interface IDispach
     {
-        
+        /**
+         * Method to construct instance of Dispach
+         *
+         * @param FcPhp\Di\Interfaces\IDi $di Instance of Di
+         * @return void
+         */
+        public function __construct(IDi $di);
+
+        /**
+         * Method to execute controller
+         *
+         * @param string $action Action to execute, ex: controller@method
+         * @param array $params Params to method
+         * @throws FcPhp\Dispach\Exceptions\DispachConfigurationErrorException
+         * @throws FcPhp\Dispach\Exceptions\DispachControllerNotFoundException
+         * @throws FcPhp\Dispach\Exceptions\DispachMethodNotFoundException
+         * @return mixed
+         */
+        public function dispach(string $action, array $params = []);
+
+        /**
+         * Method to configure callback
+         *
+         * @param string $name Name of callback
+         * @param object $callback Callback to execute
+         * @return void
+         */
+        public function callback(string $name, object $callback) :void;
     }
 }
